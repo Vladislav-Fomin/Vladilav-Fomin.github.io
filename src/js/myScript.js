7b7fa90918e0278398cbebbcd1be6551593d4125
Vladislav-Fomin <105593638+Vladislav-Fomin@users.noreply.github.com>
2022-07-17 17:39:12 +0500

    Add files via upload

--- src/js/myScript.js
+++ src/js/myScript.js
@@ -1,5 +1,6 @@
 "use strict"
 
+//preloader
 window.onload = function () {
     document.body.classList.add('loaded_hiding');
     window.setTimeout(function () {
@@ -8,129 +9,116 @@ window.onload = function () {
     }, 500);
 }
 
+//navigation
 $(window).scroll(() => {
     let scrollDistance = $(window).scrollTop();
-    
-    $(".section").each((i, el) => {
-        
-        if ($(el).offset().top - $("nav").outerHeight() <= scrollDistance){
-            $("nav a").each((i, el) => {
-                if ($(el).hasClass("active")) {
-                    $(el).removeClass("active");
+
+    $('.section').each((i, el) => {
+
+        if ($(el).offset().top - $('nav').outerHeight() <= scrollDistance) {
+            $('nav a').each((i, el) => {
+                if ($(el).hasClass('active')) {
+                    $(el).removeClass('active');
                 }
             });
-            
-            $('nav li:eq('+ i +')').find('a').addClass('active');
+
+            $('nav li:eq(' + i + ')').find('a').addClass('active');
         }
     });
 });
 
 $('a[href^="#"]').click(function () {
     let varHref = $(this).attr("href");
-    $('html, body').animate({scrollTop: $(valHref).offset().top - 20 + "px"});
+    $('html, body').animate({
+        scrollTop: $(valHref).offset().top - 20 + "px"
+    });
 });
 
-$(function() {
-    
+//lazy-loading
+const imageObserver = new IntersectionObserver(
+    (entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.src = entry.target.dataset.src;
+                observer.unobserve(entry.target);
+            }
+        });
+    }, {
+        rootMargin: '50px',
+    });
+
+document
+    .querySelectorAll("img")
+    .forEach((image) => imageObserver.observe(image));
+
+//calculate
+function calculate(){
+    let price = parseInt($("#select1 option:selected").val()) + parseInt($("#select2 option:selected").val()) + parseInt($("#select3 option:selected").val());
+    let days = parseInt($("#select1 option:selected").attr("days")) + parseInt($("#select2 option:selected").attr("days")) + parseInt($("#select3 option:selected").attr("days"));
+    $(".days .digit").text(days);
+    $(".price .digit").text(price);
+ }
+$("select").on("change", function(){
+    calculate();
+});
+calculate();
+               
+//magnific-popup
+$(document).ready(function () {
+    $('.popup-gallery').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0, 1]
+        },
+        image: {   
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+      
+            }
+    });
+});
+
+//spincrement
+$(document).ready(function () {
+    let options = {
+        threshold: [0.5]
+    };
+    let observer = new IntersectionObserver(onEntry, options);
+    let elements = $('.spincrement');
+    elements.each((i, el) => {
+        observer.observe(el);
+    });
+    function onEntry(entry) {
+        entry.forEach(change => {
+            if (change.isIntersecting) {
+                $('.spincrement').spincrement({
+                    thousandSeparator: ",",
+                    duration: 1200
+                    });
+                }
+            });
+        }
+});
+
+//slick-slider
+$(function () {
     $('.case_slider').slick({
         arrows: true,
         dots: true,
-        adaptiveHeight: true,      
-    })
-
+        adaptiveHeight: true,
+        prevArrow: "<img src='img/iconmonstr-arrow-64.svg' class='prev' alt='1'>",
+        nextArrow: "<img src='img/iconmonstr-arrow-63.svg' class='next' alt='2'>",
+    });
 });
 
-$(function() {
-    
+$(function () {
     $('.rev_slider').slick({
         arrows: false,
         dots: true,
-        adaptiveHeight: true 
-    })
-
+        adaptiveHeight: true
+    });
 });
-
-/*
-let inf = [];
-
-while (true) {
-    let siteType = prompt(`Тип сайта\n1-landing page\n2-сайт визитка\n3-корпоративный сайт`);
-    if (siteType > 0 && siteType < 4) {
-        inf.push(siteType);
-        break;
-    } else {
-        alert("Данные введены неверно");
-    }
-};
-
-while (true) {
-    let design = prompt(`Дизайн\n1-шаблонный дизайн\n2-уникальный дизайн`);
-    if (design > 0 && design < 3) {
-        inf.push(design);
-        break;
-    } else {
-        alert("Данные введены неверно");
-    }
-};
-while (true) {
-    let adaptability = prompt(`Адаптивность\n1-для всех устройств\n2-только для компьютров\n3-только для смартфонов`);
-    if (adaptability > 0 && adaptability < 4) {
-        inf.push(adaptability);
-        break;
-    } else {
-        alert("Данные введены неверно");
-    }
-};
-
-
-let price = [];
-let terms = [];
-
-if (inf[0] == 1) {
-    price.push(1000);
-    terms.push(1);
-} 
-else if (inf[0] == 2) {
-    price.push(3000);
-    terms.push(2);
-} 
-else {
-    price.push(5000);
-    terms.push(4);
-}
-if (inf[1] == 1) {
-    price.push(1000);
-    terms.push(2);
-} 
-else {
-    price.push(3000);
-    terms.push(4);
-} 
-if (inf[2] == 1) {
-    price.push(5000);
-    terms.push(4);
-} 
-else if (inf[2] == 2) {
-    price.push(2000);
-    terms.push(2);
-} 
-else {
-    price.push(3000);
-    terms.push(2);
-}
-
-
-let sumPrice;
-function calcPrice(){
-    sumPrice = price[0] + price[1] + price[2];
-}
-calcPrice(price[0], price[1], price[2]);
-
-let sumTerms;
-function calcTerms(){
-    sumTerms = terms[0] +terms[1] + terms[2];
-}
-calcTerms(terms[0], terms[1], terms[2]);
-
-alert(`Стоимость: `  + (sumPrice) + `\nСроки: ` + (sumTerms));
-*/
